e32b0a7db4aec2aac1d9c4aa6cf40d46faf51c3f
redouane <redouaneboukamir@gmail.com>
2019-04-17 09:53:51 +0200

    setting the login problem

--- src/Controller/securityController.php
+++ src/Controller/securityController.php
@@ -6,15 +6,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class securityController extends AbstractController{
 
     /**
      * @Route("/login", name="login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('home/login.html.twig')
+        $last_username = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+        return $this->render('home/login.html.twig', [
+            'last_username' => $last_username,
+            'error' => $error
+        ])
 ;
     }
 
