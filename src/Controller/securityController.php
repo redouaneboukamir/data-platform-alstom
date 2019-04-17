42f8637505d692a777fb6471367e78393702b2ce
redouane <redouaneboukamir@gmail.com>
2019-04-17 15:08:48 +0200

    Save

--- src/Controller/securityController.php
+++ src/Controller/securityController.php
@@ -20,10 +20,15 @@ class securityController extends AbstractController{
         $error = $authenticationUtils->getLastAuthenticationError();
 
         if (TRUE === $this->get('security.authorization_checker')->isGranted('ROLE_ALSTOM')){
+
             return $this->redirectToRoute('alstom.home');
+
         }else if (TRUE === $this->get('security.authorization_checker')->isGranted('ROLE_CLIENT')){
+
             return $this->redirectToRoute('client.home');
+
         }else{
+
             return $this->render('home/login.html.twig', [
                 'last_username' => $last_username,
                 'error' => $error
